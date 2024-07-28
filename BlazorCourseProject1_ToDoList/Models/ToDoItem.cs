9c1bdec02cd4db3a69b28b3e1064e7b213fa281d
Francis Khoury <fkhoury@bn.com>
2024-07-28 11:33:46 -0400

    Initial Version

---
+++ BlazorCourseProject1_ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ToDoListProject.Models
+{
+    public class ToDoItem
+    {
+        public ToDoItem()
+        {
+            Name = "New Task";
+            CreatedTime = DateTime.Now;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        private bool _isComplete;
+
+        public bool IsComplete
+        {
+            get => _isComplete;
+            set
+            {
+                _isComplete = value;
+                if (value)
+                {
+                    CompletedTime = DateTime.Now;
+                }
+            }
+        }
+        public DateTime CreatedTime { get; private set; }
+        public DateTime? CompletedTime { get; set; }
+    }
+}
---
+++ BlazorCourseProject1_ToDoList/Models/ToDoRepo.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using System.Threading.Tasks;
+
+namespace ToDoListProject.Models
+{
+    public static class ToDoRepo
+    {
+        private static List<ToDoItem> tasks = new List<ToDoItem>()
+        {
+            new ToDoItem { Id=1, Name = "Task numbah 1", IsComplete=false }
+        };
+
+        public static void AddTask()
+        {
+            ToDoItem task = new ToDoItem();
+            var maxId = tasks.Max(t => t.Id);
+            task.Id = maxId + 1;
+            tasks.Add(task);
+            //tasks = tasks.OrderBy(t => t.IsComplete.ToString()).Reverse().ToList();
+        }
+
+        public static List<ToDoItem> GetTasks()
+        {
+            var sortedTasks = tasks.
+                OrderBy(t => t.IsComplete)
+                .ThenByDescending(t => t.Id)
+                .ToList();
+            return sortedTasks;
+        }
+
+        public static ToDoItem? GetTaskById(int id)
+        {
+            var task = tasks.FirstOrDefault(s => s.Id == id);
+            if (task != null)
+            {
+                return new ToDoItem
+                {
+                    Id = task.Id,
+                    Name = task.Name,
+                    IsComplete = task.IsComplete,
+                    CompletedTime = task.CompletedTime
+                };
+            }
+            return null;
+        }
+
+    }
+}
