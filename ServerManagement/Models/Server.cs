bef7cc309fc80fefb3cc92d6cf9998fc261ef043
Francis Khoury <fkhoury@bn.com>
2024-07-31 16:28:13 -0400

    Fix Assignment 7 User Counts

--- ServerManagement/Models/Server.cs
+++ ServerManagement/Models/Server.cs
@@ -4,7 +4,7 @@ namespace ServerManagement.Models
 {
     public class Server
     {
-        private bool _isOnline = false;
+        //private bool _isOnline = false;
 
         public Server()
         {
@@ -13,22 +13,7 @@ namespace ServerManagement.Models
             IsOnline = randomNumber == 0;
         }
         public int ServerId { get; set; }
-        public bool IsOnline {
-            get => _isOnline;
-            set {
-                if (value)
-                {
-                    Random random = new Random();
-                    int randomNumber = random.Next(0, 1000);
-                    UserCount = randomNumber;
-                }
-                else
-                {
-                    UserCount = 0;
-                }
-                _isOnline = value;
-
-            } }
+        public bool IsOnline { get; set; }
 
         [Required]
         public string? Name { get; set; }
@@ -37,5 +22,6 @@ namespace ServerManagement.Models
         public string? City { get; set; }
 
         public int UserCount { get; set; }
+
     }
 }
