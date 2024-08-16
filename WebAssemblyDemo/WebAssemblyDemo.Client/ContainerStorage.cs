1dc0a4e00041c5f189de591b4122f767b65231a1
Francis Khoury <fkhoury@bn.com>
2024-08-16 09:56:58 -0400

    State storage using ContainerStorage (DI) class in WASM

---
+++ WebAssemblyDemo/WebAssemblyDemo.Client/ContainerStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAssemblyDemo.Client
+{
+    public class ContainerStorage
+    {
+        private string _message = String.Empty;
+
+        public string GetMessage() { return _message; }
+
+        public void SetMessage(string msg) { _message = msg; }
+    }
+}
